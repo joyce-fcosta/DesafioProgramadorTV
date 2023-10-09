9e2165ad4b38e67eb342bec8d3d269291b019a74
joyce-fcosta <joyced.fcosta@gmail.com>
2023-10-09 11:48:27 -0300

    Commit inicial

---
+++ DesafioProgTV/Models/InformacoesVideo.cs
@@ -0,0 +1,11 @@
+﻿namespace DesafioProgTV.Models
+{
+	public class InformacoesVideo
+	{
+		public string? Tamanho { get; set; }
+		public string? Nome { get; set; }
+		public string? DataDeCriacao { get; set; }
+		public string? Extensao { get; set; }
+		public string? DiretorioSalvo { get; set; }
+	}
+}
---
+++ DesafioProgTV/Services/VideoService.cs
@@ -0,0 +1,37 @@
+﻿using DesafioProgTV.Models;
+using System.IO;
+using System.Text;
+using System.Windows;//.Storage.StorageFile;
+namespace DesafioProgTV.Services
+{
+    public class VideoService
+    {
+
+        public InformacoesVideo ObtemInfoVideos(string caminho)
+        {
+
+
+            var diretorios = Directory.EnumerateFiles(caminho);
+
+            InformacoesVideo video = new InformacoesVideo();
+
+            foreach (var item in diretorios)
+            {
+                var arquivo = new FileInfo(caminho);
+                video.DiretorioSalvo = arquivo.DirectoryName;
+                video.Nome = arquivo.Name;
+                video.DataDeCriacao = arquivo.CreationTime.ToString("dd/MM/yyyy");
+                video.Extensao = arquivo.Extension;
+                video.Tamanho = arquivo.Length.ToString();
+            }
+
+            return video;
+        }
+
+        public void SalvaVideo(string caminho)
+        {
+
+        }
+
+    }
+}
